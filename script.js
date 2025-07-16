8f1f1480697032bc4190ba02041bdf79a67af578
PongTon420 <tututien123@gmail.com>
2025-07-16 14:24:49 +0700

    minor update: cleaner code, add clear all button

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-//Main Generator
 const generatorForm = document.getElementById('generator-form');
 const resultParagraph = document.getElementById('result-paragraph');
 
-function paragraphResultChange(paragraph, name, age, place, isMarried)
+//Remember to add new input to formFields
+const formFields =
+{   
+    name: generatorForm.elements['name'],
+    age: generatorForm.elements['age'],
+    house: generatorForm.elements['house'],
+    isMarried: generatorForm.elements['is-married'],
+}
+
+function paragraphResultChange(paragraph, name, age, house, isMarried)
 {
     paragraph.textContent = 
            `“My name is ${name}. I\'m ${age} years old.
-            My house is in ${place}, where all the villas are, and I am ${isMarried}.
+            My house is in ${house}, where all the villas are, and I am ${isMarried}.
             I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest.
             I don\'t smoke, but I occasionally drink.
             I\'m in bed by 11 PM, and make sure I get eight hours of sleep, no matter what.
@@ -20,20 +28,10 @@ function paragraphResultChange(paragraph, name, age, place, isMarried)
             Although, if I were to fight, I wouldn\'t lose to anyone.”`;
 }
 
-//Random Everything
-const formFields =
-{   //remember to add new input to formFields
-    name: generatorForm.elements['name'],
-    age: generatorForm.elements['age'],
-    place: generatorForm.elements['place'],
-    isMarried: generatorForm.elements['is-married'],
-}
-
 const allInputsExceptRandomEverything = generatorForm.querySelectorAll("input:not(#random-everything)")
-
-function disableAllInputs(allInputsArray, disabled)
+function disableAllInputs(InputsArray, disabled)
 {
-    allInputsArray.forEach(function(input)
+    InputsArray.forEach(function(input)
     {
         input.disabled = disabled;
     })
@@ -42,62 +40,70 @@ function disableAllInputs(allInputsArray, disabled)
 function getRandomFromArray(array) 
 {
   return array[Math.floor(Math.random() * array.length)];
-  //Math.floor = round down (2.9->2)
-  //Math.random() = random between 0-1 (e.g. 0.22, 0.99)
-  // * arry.length -> random between 0-array.length (e.g. 4 items = 0-1-2-3 get rounded down)
+  //Math.floor -> round down (2.9->2)
+  //Math.random() -> random between 0-1 (e.g. 0.22, 0.99)
+  //* arry.length -> random between 0-array.length (e.g. 4 items = 0-1-2-3 get rounded down)
 }
 
 const randomEverythingChecked = document.getElementById('random-everything');
 
-function disableInputsWhenRandomEverythingChecked()
-{
-if(randomEverythingChecked.checked)
-{
-    disableAllInputs(allInputsExceptRandomEverything, true);
-}
-else
-{
-    disableAllInputs(allInputsExceptRandomEverything, false);
-}
-}
-
+randomEverythingChecked.addEventListener("change", function()
+    {   
+        console.log('RandomEverything Checkbox changed');
+        if (randomEverythingChecked.checked)
+            disableAllInputs(allInputsExceptRandomEverything, true);
+        else
+            disableAllInputs(allInputsExceptRandomEverything, false);
+    }
+);
 
 function generatorFormInput(event)
     {
         event.preventDefault() //ngăn reload page
+        console.log('Submit button was pressed');
         if (randomEverythingChecked.checked)
         {   
             let randomList = {}
             fetch("randomList.json")
             .then(response => response.json())
-            .then(json => {randomList = json;
+            .then(json => {
+                
+            randomList = json; //load xong file json trước
+
             formFields.name.value = getRandomFromArray(randomList.name);
             formFields.age.value = getRandomFromArray(randomList.age);
-            formFields.place.value = getRandomFromArray(randomList.place);
+            formFields.house.value = getRandomFromArray(randomList.house);
             formFields.isMarried.value = getRandomFromArray(randomList.isMarried);
 
             let name = formFields.name.value.trim();
             let age = formFields.age.value.trim();
-            let place = formFields.place.value.trim();
+            let house = formFields.house.value.trim();
             let isMarried = formFields.isMarried.value.trim();
             
-            paragraphResultChange(resultParagraph, name, age, place, isMarried);
+            paragraphResultChange(resultParagraph, name, age, house, isMarried);
+            
             });
         }
         else
         {
-            const formData = new FormData(generatorForm);
+            let name = formFields.name.value.trim() || "Yoshikage Kira";
+            let age = formFields.age.value.trim() || "33";
+            let house = formFields.house.value.trim() || "the northeast section of Morioh";
+            let isMarried = formFields.isMarried.value.trim();
 
-            let name = formData.get('name').trim() || "Yoshikage Kira";
-            let age = formData.get('age') || "33";
-            let place = formData.get('place').trim() || "the northeast section of Morioh";
-            let isMarried = formData.get('is-married').trim();
-
-            paragraphResultChange(resultParagraph, name, age, place, isMarried);
+            paragraphResultChange(resultParagraph, name, age, house, isMarried);
         }
     };
 
-randomEverythingChecked.addEventListener("change", disableInputsWhenRandomEverythingChecked);
+//Event 'change' when 'reset'
+generatorForm.addEventListener('reset', function (event) 
+    {
+        console.log('Reset button was pressed');
+        setTimeout(function()
+            {
+                randomEverythingChecked.dispatchEvent(new Event('change'));
+            }, 0);
+    }
+);
 
 generatorForm.addEventListener('submit', generatorFormInput);
-
