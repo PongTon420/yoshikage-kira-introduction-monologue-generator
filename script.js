c7e64b401adfc9b0dfd9fae7753a245cfce4f8b8
PongTon420 <tututien123@gmail.com>
2025-07-21 21:47:58 +0700

    Rewrite most of the code and add lots of translations

--- script.js
+++ script.js
@@ -2,7 +2,43 @@ const generatorForm = document.getElementById('generator-form');
 const generateButton = document.getElementById('generateButton');
 const translateButton = document.getElementById('translateButton');
 const resultParagraph = document.getElementById('result-paragraph');
+translateButton.disabled = true;
 
+let rName;
+let rAge;
+let rHouse;
+let rIsMarried;
+let rWorktitle;
+let rWorkplace;
+let rTimeGetHome ;
+let rTimeToSleep;
+let rNotDo;
+let rDo;
+let rWish;
+
+let Name;
+let age;
+let house;
+let isMarried;
+let worktitle;
+let workplace;
+let timeGetHome;
+let timeToSleep;
+let notDo;
+let Do;
+let wish;
+
+let tName;
+let tAge;
+let tHouse;
+let tIsMarried;
+let tWorktitle;
+let tWorkplace;
+let tTimeGetHome;
+let tTimeToSleep;
+let tNotDo;
+let tDo;
+let tWish;
 
 //Remember to add new input to formFields
 const formFields =
@@ -20,11 +56,11 @@ const formFields =
     wish: generatorForm.elements['wish']
 }
 
-function paragraphResultChange(paragraph, name, age, house, isMarried, worktitle, workplace,
+function paragraphResultChange(paragraph, Name, age, house, isMarried, worktitle, workplace,
     timeGetHome, timeToSleep, notDo, Do, wish)
 {
     paragraph.textContent = 
-           `“My name is ${name}. I\'m ${age} years old.
+           `“My name is ${Name}. I\'m ${age} years old.
             My house is in ${house}, where all the villas are, and I am ${isMarried}.
             I work as ${worktitle} for ${workplace}, and I get home every day by ${timeGetHome} at the latest.
             I don\'t ${notDo}, but I occasionally ${Do}.
@@ -74,70 +110,106 @@ randomEverythingChecked.addEventListener("change", function()
     }
 );
 
-function generatorFormInput()
+let randomList = [];
+
+async function loadRandomList() {
+    const response = await fetch("randomList.json");
+    const json = await response.json();
+    randomList = json;
+
+    rName = getRandomFromArray(randomList.name);
+    rAge = getRandomFromArray(randomList.age);
+    rHouse = getRandomFromArray(randomList.house);
+    rIsMarried = getRandomFromArray(randomList.isMarried);
+    rWorkplace = getRandomFromArray(randomList.workplace);
+    rWorktitle = getRandomFromArray(randomList.worktitle);
+    rTimeGetHome = getRandomFromArray(randomList.timeGetHome);
+    rTimeToSleep = getRandomFromArray(randomList.timeGetHome);
+    rNotDo = getRandomFromArray(randomList.notDo);
+    rDo = getRandomFromArray(randomList.Do);
+    rWish = getRandomFromArray(randomList.wish);
+
+    Name = formFields.name.value.trim() || "Yoshikage Kira";
+    age = formFields.age.value.trim() || "33";
+    house = formFields.house.value.trim() || "the northeast section of Morioh";
+    isMarried = formFields.isMarried.value.trim();
+    worktitle = formFields.worktitle.value.trim() || "an employee";
+    workplace = formFields.workplace.value.trim() || "the Kame Yu department stores";
+    timeGetHome = formFields.timeGetHome.value.trim();
+    timeToSleep = formFields.timeToSleep.value.trim();
+    notDo = formFields.notDo.value.trim() || "smoke";
+    Do = formFields.Do.value.trim() || "drink";
+    wish = formFields.wish.value.trim() || "to live a very quiet life";
+}
+
+async function generateResultParagraph()
 {
+    await loadRandomList();
     console.log('Generate button was pressed');
-    //load xong file json trước
-    let randomList = {}
-        fetch("randomList.json")
-        .then(response => response.json())
-        .then(json => 
-        {      
-            randomList = json;
+    if (randomEverythingChecked.checked)
+    {                       
+        paragraphResultChange(resultParagraph, rName, rAge, rHouse, rIsMarried, rWorktitle, rWorkplace,
+                rTimeGetHome, rTimeToSleep, rNotDo, rDo, rWish);
 
-            let rName = getRandomFromArray(randomList.name);
-            let rAge = getRandomFromArray(randomList.age);
-            let rHouse = getRandomFromArray(randomList.house);
-            let rIsMarried = getRandomFromArray(randomList.isMarried);
-            let rWorkplace = getRandomFromArray(randomList.workplace);
-            let rWorktitle = getRandomFromArray(randomList.worktitle);
-            let rTimeGetHome = getRandomFromArray(randomList.timeGetHome);
-            let rTimeToSleep = getRandomFromArray(randomList.timeGetHome);
-            let rNotDo = getRandomFromArray(randomList.notDo);
-            let rDo = getRandomFromArray(randomList.Do);
-            let rWish = getRandomFromArray(randomList.wish);
+        tName = rName;
+        tAge = rAge;
+        tHouse = rHouse;
+        tIsMarried = rIsMarried;
+        tWorktitle = rWorktitle;
+        tWorkplace = rWorkplace;
+        tTimeGetHome = rTimeGetHome;
+        tTimeToSleep = rTimeToSleep;
+        tNotDo = rNotDo;
+        tDo = rDo;
+        tWish = rWish;
+    }
+    else if (randomIfBlank.checked)
+    {
+        Name = formFields.name.value.trim() || rName;
+        age = formFields.age.value.trim() || rAge;
+        house = formFields.house.value.trim() || rHouse;
+        isMarried = isMarried;
+        workplace = formFields.workplace.value.trim() || rWorkplace;
+        worktitle = formFields.worktitle.value.trim() || rWorktitle;
+        timeGetHome = timeGetHome;
+        timeToSleep = timeToSleep;
+        notDo = formFields.notDo.value.trim() || rNotDo;
+        Do = formFields.Do.value.trim() || rDo;
+        wish = formFields.wish.value.trim() || rWish;
 
-            let name = formFields.name.value.trim() || "Yoshikage Kira";
-            let age = formFields.age.value.trim() || "33";
-            let house = formFields.house.value.trim() || "the northeast section of Morioh";
-            let isMarried = formFields.isMarried.value.trim();
-            let worktitle = formFields.worktitle.value.trim() || "an employee";
-            let workplace = formFields.workplace.value.trim() || "the Kame Yu department stores";
-            let timeGetHome = formFields.timeGetHome.value.trim();
-            let timeToSleep = formFields.timeToSleep.value.trim();
-            let notDo = formFields.notDo.value.trim() || "smoke";
-            let Do = formFields.Do.value.trim() || "drink";
-            let wish = formFields.wish.value.trim() || "to live a very quiet life";
+        paragraphResultChange(resultParagraph, Name, age, house, isMarried, worktitle, workplace,
+                timeGetHome, timeToSleep, notDo, Do, wish);
 
-            if (randomEverythingChecked.checked)
-            {                       
-                paragraphResultChange(resultParagraph, rName, rAge, rHouse, rIsMarried, rWorktitle, rWorkplace,
-                        rTimeGetHome, rTimeToSleep, rNotDo, rDo, rWish);
-            }
-            else if (randomIfBlank.checked)
-            {
-                name = formFields.name.value.trim() || rName;
-                age = formFields.age.value.trim() || rAge;
-                house = formFields.house.value.trim() || rHouse;
-                isMarried = isMarried;
-                workplace = formFields.workplace.value.trim() || rWorkplace;
-                worktitle = formFields.worktitle.value.trim() || rWorktitle;
-                timeGetHome = timeGetHome;
-                timeToSleep = timeToSleep;
-                notDo = formFields.notDo.value.trim() || rNotDo;
-                Do = formFields.Do.value.trim() || rDo;
-                wish = formFields.wish.value.trim() || rWish;
- 
-                paragraphResultChange(resultParagraph, name, age, house, isMarried, worktitle, workplace,
-                        timeGetHome, timeToSleep, notDo, Do, wish);
-            }
+        tName = Name;
+        tAge = age;
+        tHouse = house;
+        tIsMarried = isMarried;
+        tWorktitle = worktitle;
+        tWorkplace = workplace;
+        tTimeGetHome = timeGetHome;
+        tTimeToSleep = timeToSleep;
+        tNotDo = notDo;
+        tDo = Do;
+        tWish = wish;
+    }
+    else
+    {
+        paragraphResultChange(resultParagraph, Name, age, house, isMarried, worktitle, workplace,
+                timeGetHome, timeToSleep, notDo, Do, wish);
 
-            else
-            {
-                paragraphResultChange(resultParagraph, name, age, house, isMarried, worktitle, workplace,
-                        timeGetHome, timeToSleep, notDo, Do, wish);
-            }
-        });
+        tName = Name;
+        tAge = age;
+        tHouse = house;
+        tIsMarried = isMarried;
+        tWorktitle = worktitle;
+        tWorkplace = workplace;
+        tTimeGetHome = timeGetHome;
+        tTimeToSleep = timeToSleep;
+        tNotDo = notDo;
+        tDo = Do;
+        tWish = wish;
+    }
+    translateButton.disabled = false;
 };
 
 //Event 'change' when 'reset'
@@ -148,42 +220,139 @@ generatorForm.addEventListener('reset', function (event)
             {
                 randomEverythingChecked.dispatchEvent(new Event('change'));
                 paraReset(resultParagraph);
+                translateButton.disabled = true;
             }, 0);
         
     }
 );
 
-function translateVN(paragraph)
+function translateVN(paragraph, Name, age, house, isMarried, worktitle, workplace,
+                timeGetHome, timeToSleep, notDo, Do, wish)
 {
-    let name = formFields.name.value.trim() || "Yoshikage Kira";
-    let age = formFields.age.value.trim() || "33";
-    let house = formFields.house.value.trim() || "the northeast section of Morioh";
-    let isMarried = formFields.isMarried.value.trim();
-    let worktitle = formFields.worktitle.value.trim() || "an employee";
-    let workplace = formFields.workplace.value.trim() || "the Kame Yu department stores";
-    let timeGetHome = formFields.timeGetHome.value.trim();
-    let timeToSleep = formFields.timeToSleep.value.trim();
-    let notDo = formFields.notDo.value.trim() || "smoke";
-    let Do = formFields.Do.value.trim() || "drink";
-    let wish = formFields.wish.value.trim() || "to live a very quiet life";
-
     if (isMarried === "not married"){ isMarried = "chưa kết hôn"} 
     else { isMarried = "đã kết hôn"};
+    switch(house)
+    {
+        case "the northeast section of Morioh":
+            house = "khu đông bắc Morioh";
+            break;
+        case "Rach Gia City":
+            house = "Thành phố Rạch Giá";
+            break;
+        case "Rome":
+            house = "La Mã";
+            break;
+        case "Hanoi":
+            house = "Hà Nội";
+            break;
+        case "Ho Chi Minh City":
+            house = "Thành phố Hồ Chí Minh";
+            break;
+        case "Hong Kong":
+            house = "Hồng Kông";
+            break;
+        case "Macau":
+            house = "Ma Cao";
+            break;
+        case "Taipei":
+            house = "Đài Bắc";
+            break;
+        case "Beijing":
+            house = "Bắc Kinh";
+            break;
+        case "Shanghai":
+            house = "Thượng Hải";
+            break;
+        case "Chengdu":
+            house = "Thành Đô";
+            break;
+        case "Xian":
+            house = "Tây An";
+            break;
+        case "Tokyo Tower":
+            house = "Tháp Tokyo";
+            break;
+        case "Konoha":
+            house = "Làng Lá";
+            break;
+        case "Gotham City":
+            house = "Thành phố Gotham";
+            break;
+        case "Isekai":
+            house = "Thế giới khác";
+            break;
+        case "The City":
+            house = "Thành phố";
+            break;
+        case "Fuuto City":
+            house = "Thành phố Fuuto";
+            break;
+        case "Ancient Rome":
+            house = "La Mã cổ đại";
+            break;
+    };
+    switch (worktitle)
+    {
+        case "an employee":
+            worktitle = "nhân viên";
+            break;
+        case "a janitor":
+            worktitle = "lao công";
+            break;
+        case "a wizard":
+            worktitle = "pháp sư";
+            break;
+        case "a dog":
+            worktitle = "con chó";
+            break;
+        case "a project manager":
+            worktitle = "người quản lý dự án";
+            break;
+        case "a Master":
+            worktitle = "Master";
+            break;
+        case "a Youtuber":
+            worktitle = "Youtuber";
+            break;
+        case "a doctor":
+            worktitle = "bác sĩ";
+            break;
+        case "S-rank Hero":
+            worktitle = "anh hùng S-rank";
+            break;
+        case "No. 1 Hero":
+            worktitle = "anh hùng hạng nhất";
+            break;
+    };
+    
     paragraph.textContent = 
-           `“Tôi tên là ${name}. Tôi ${age} tuổi.
-            Nhà tôi ở ${house}, nơi có nhiều biệt thự, và tôi chưa kết hôn.
-            Tôi làm nhân viên tại cửa hàng bách hóa Kame Yu, và tôi luôn về đến nhà trước 8 giờ tối.
-            Tôi không hút thuốc, nhưng thỉnh thoảng có uống rượu.
-            Tôi đi ngủ lúc 11 giờ tối và luôn đảm bảo ngủ đủ tám tiếng mỗi đêm, bất kể chuyện gì.
-            Trước khi ngủ, tôi uống một ly sữa ấm và tập giãn cơ khoảng 20 phút,
-            nên tôi thường không gặp vấn đề gì khi ngủ thẳng tới sáng.
-            Giống như một đứa trẻ, tôi thức dậy mà không cảm thấy mệt mỏi hay căng thẳng.
-            Bác sĩ bảo rằng lần kiểm tra sức khỏe gần nhất của tôi không có gì bất thường.
-            Tôi muốn nói rằng tôi là người chỉ mong có một cuộc sống yên bình.
-            Tôi tránh xa mọi rắc rối, những thứ như thắng thua hay kẻ thù, vì chúng sẽ làm tôi mất ngủ.
-            Đó là cách tôi sống giữa xã hội, và tôi biết chính điều đó đem lại hạnh phúc cho tôi.
-            Tuy nhiên, nếu phải chiến đấu, tôi sẽ không thua bất kỳ ai.”`;
+           `“Tên tôi là ${Name}. Năm nay ${age} tuổi. Nhà của tôi nằm ở ${house}, 
+            nơi toàn những biệt thự, và tôi chưa kết hôn. Tôi làm ${worktitle} cho ${workplace}
+            và luôn là người về nhà muộn nhất lúc 8 giờ. Tôi không hút thuốc, nhưng thỉnh thoảng có uống rượu. 
+            Tôi đi ngủ lúc 11 giờ tối, luôn đảm bảo ngủ đủ 8 tiếng, bất kì chuyện gì đi nữa. Sau khi uống một ly sữa ấm 
+            và tập thể dục nhẹ 20 phút trước khi lên giường, tôi sẽ ngủ ngon đến sáng mà không gặp vấn đề gì. 
+            Như một đứa trẻ, tôi luôn thức dậy, đầy sảng khoái và không chút mỏi mệt. Lần khám bệnh cuối của 
+            tôi cũng cho thấy tôi hoàn toàn khỏe mạnh.
+            Tôi đang cố giải thích rằng tôi là một kẻ chỉ muốn sống một cuộc đời bình yên phẳng lặng, 
+            không mảy may đến chuyện thắng thua với ai, đặc biệt khi nó làm tôi mất ngủ vào buổi đêm. 
+            Đó là cách tôi đương đầu với cái xã hội này, và tôi biết điều đó làm tôi cảm thấy hạnh phúc hơn hết.
+            Tuy nhiên, nếu phải chiến đấu với bất kỳ kẻ nào, tôi nhất định sẽ không thua!”`;
+
+            /*Tên tôi là Yoshikage Kira. Năm nay 33 tuổi. Nhà của tôi nằm ở khu Đông Bắc của Morioh, 
+            nơi toàn những biệt thự, và tôi chưa kết hôn. Tôi làm nhân viên tại cửa hàng Kame Yu
+            và luôn là người về nhà muộn nhất lúc 8 giờ. Tôi không hút thuốc, nhưng thỉnh thoảng có uống rượu. 
+            Tôi đi ngủ lúc 11 giờ tối, luôn đảm bảo ngủ đủ 8 tiếng, bất kì chuyện gì đi nữa. Sau khi uống một ly sữa ấm 
+            và tập thể dục nhẹ 20 phút trước khi lên giường, tôi sẽ ngủ ngon đến sáng mà không gặp vấn đề gì. 
+            Như một đứa trẻ, tôi luôn thức dậy, đầy sảng khoái và không chút mỏi mệt. Lần khám bệnh cuối của 
+            tôi cũng cho thấy tôi hoàn toàn khỏe mạnh.
+            Tôi đang cố giải thích rằng tôi là một kẻ chỉ muốn sống một cuộc đời bình yên phẳng lặng, 
+            không mảy may đến chuyện thắng thua với ai, đặc biệt khi nó làm tôi mất ngủ vào buổi đêm. 
+            Đó là cách tôi đương đầu với cái xã hội này, và tôi biết điều đó làm tôi cảm thấy hạnh phúc hơn hết.
+            Tuy nhiên, nếu phải chiến đấu với bất kỳ kẻ nào, tôi nhất định sẽ không thua! */
 }
 
-generateButton.addEventListener('click', generatorFormInput);
-translateButton.addEventListener('click', () => {translateVN(resultParagraph)}); 
+generateButton.addEventListener('click', generateResultParagraph);
+translateButton.addEventListener('click', () => {translateVN(
+    resultParagraph, tName, tAge, tHouse, tIsMarried, tWorktitle, tWorkplace, tTimeGetHome, tTimeToSleep, 
+    tNotDo, tDo, tWish)
+}); 
